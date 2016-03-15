2b17b129fe6158b871ffe27bb4fd40e1ceb3c052
Dejan Kitic <dejan.kitic@ericsson.com>
2016-03-15 14:31:37 +0000

    Working modal response

--- swarm-application/frontend/js/send-payments.js
+++ swarm-application/frontend/js/send-payments.js
@@ -1,17 +1,18 @@
 $("form").submit(function (event) {
-    var data_array = $('#form').serializeArray().reduce(function(obj, item) {
+    var data_array = $('#form').serializeArray().reduce(function (obj, item) {
         obj[item.name] = item.value;
         return obj;
     }, {});
 
     $.ajax({
-        url:"http://172.17.0.3:8080/api/v1.0/payment",
-        type:"POST",
-        data:JSON.stringify(data_array),
-        contentType:"application/json; charset=utf-8",
-        dataType:"json",
-        success: function(){
-            $('#myModal').modal('show');
+        url: "http://172.17.0.3:8080/api/v1.0/payment",
+        type: "POST",
+        data: JSON.stringify(data_array),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            $('#tracking_number').html("Tracking identification: <strong>"+data['message']+"</strong>");
+            $('#myModal').modal('show')
         }
     });
     event.preventDefault();
