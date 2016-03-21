518c6ea3fa9a354bd9d85e94b8f51ee5514dd276
Dejan Kitic <dejan.kitic@ericsson.com>
2016-03-21 15:39:25 +0000

    Angular example frontend with bootstrap

--- frontend/app/view1/view1.js
+++ frontend/app/view1/view1.js
@@ -1,16 +1,9 @@
 'use strict';
 
 angular.module('myApp.view1', ['ngRoute'])
-
-    .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/view1', {
-            templateUrl: 'view1/view1.html',
-            controller: 'View1Ctrl'
-        });
-    }])
-
     .controller('View1Ctrl', View1Ctrl);
 View1Ctrl.$inject = ['$rootScope'];
+
 function View1Ctrl($rootScope) {
-    $rootScope.layout = "app"
+    $rootScope.layout = "app";
 }
