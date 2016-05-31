5e05ff679162ac38a2f7a94c73af6a29fc39bc7f
Dejan Kitic <dejan.kitic@ericsson.com>
2016-05-31 09:18:45 +0100

    Updated to headers.

--- swarm-demo-app/restful-demo-application-ejb-cdi-jpa/src/main/java/io/dekstroza/github/jee7/swarmdemo/app/services/SecurityInterceptor.java
+++ swarm-demo-app/restful-demo-application-ejb-cdi-jpa/src/main/java/io/dekstroza/github/jee7/swarmdemo/app/services/SecurityInterceptor.java
@@ -1,17 +1,3 @@
-/*
- * ------------------------------------------------------------------------------
- *  *******************************************************************************
- *  * COPYRIGHT Ericsson 2012
- *  *
- *  * The copyright to the computer program(s) herein is the property of
- *  * Ericsson Inc. The programs may be used and/or copied only with written
- *  * permission from Ericsson Inc. or in accordance with the terms and
- *  * conditions stipulated in the agreement/contract under which the
- *  * program(s) have been supplied.
- *  *******************************************************************************
- *  *----------------------------------------------------------------------------
- */
-
 package io.dekstroza.github.jee7.swarmdemo.app.services;
 
 import static io.dekstroza.github.jee7.swarmdemo.app.endpoints.ApplicationConstants.SUPER_SECRET_KEY;
