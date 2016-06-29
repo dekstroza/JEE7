c43640f9f39545d1ad375aefcb0fd9f47fd12ec9
Dejan Kitic <dejan.kitic@ericsson.com>
2016-06-29 15:32:32 +0100

    Added another loging implementation with functional approach.

--- swarm-demo-app/restful-demo-application-ejb-cdi-jpa-metrics/src/main/java/io/dekstroza/github/jee7/swarmdemo/app/endpoints/ApplicationLoginEndpoint.java
+++ swarm-demo-app/restful-demo-application-ejb-cdi-jpa-metrics/src/main/java/io/dekstroza/github/jee7/swarmdemo/app/endpoints/ApplicationLoginEndpoint.java
@@ -9,6 +9,7 @@ import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
 import javax.interceptor.Interceptors;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -20,8 +21,13 @@ import javax.ws.rs.core.MediaType;
 
 import io.dekstroza.github.jee7.swarmdemo.app.ProfilingInterceptor;
 import io.dekstroza.github.jee7.swarmdemo.app.api.AbstractApplicationLoginEndpoint;
+import io.dekstroza.github.jee7.swarmdemo.app.api.ApplicationUser;
 import io.dekstroza.github.jee7.swarmdemo.app.api.Credentials;
 import io.dekstroza.github.jee7.swarmdemo.app.api.InvalidCredentialsException;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
 
 @Stateless
 @Path("v1.0.0")
@@ -50,6 +56,53 @@ public class ApplicationLoginEndpoint extends AbstractApplicationLoginEndpoint {
         }
 
     }
+
+    @PermitAll
+    @Path("flogin")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Asynchronous
+    public void loginFunctional(@QueryParam(USERNAME) final String username, @QueryParam(PASSWORD) final String password,
+                      final @Suspended AsyncResponse response) {
+        try {
+            response.resume(status(OK).header(AUTHORIZATION, authenticate(new Credentials(username, password))).build());
+        } catch (final InvalidCredentialsException ie) {
+            response.resume(status(BAD_REQUEST).entity(ie.getMessage()).build());
+        } catch (final Exception e) {
+            response.resume(status(INTERNAL_SERVER_ERROR).build());
+        }
+
+    }
+
+    String authenticate(final Credentials credentials) throws InvalidCredentialsException {
+        try {
+            return generateToken(credentials).thenCombineAsync(findAppUser(credentials), (jwToken, appUser) -> {
+                return jwToken;
+            }).get();
+
+        } catch (final CompletionException ce) {
+            if (ce.getCause() instanceof NoResultException) {
+                throw new InvalidCredentialsException(ce.getCause().getMessage());
+            } else
+                throw new RuntimeException(ce.getCause());
+        } catch (final ExecutionException | InterruptedException exExc) {
+            throw new RuntimeException(exExc.getMessage());
+        }
+    }
+
+    CompletableFuture<String> generateToken(final Credentials credentials) {
+        return new CompletableFuture<String>().supplyAsync(() -> {
+            return credentials.generateJWToken();
+        });
+
+    }
+
+    CompletableFuture<ApplicationUser> findAppUser(Credentials credentials) {
+        return new CompletableFuture<ApplicationUser>().supplyAsync(() -> {
+            return findApplicationUserByCredentials(credentials);
+        });
+    }
+
 
     @Override
     protected EntityManager getEntityManager() {
