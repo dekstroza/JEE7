2d3e87a2535a56da40635df82dccf8a167ffd7e8
Dejan Kitic <dejan.kitic@ericsson.com>
2017-04-21 00:17:26 +0100

    Modules reorg

---
+++ hops-factory/order-service-module/order-service/src/main/java/com/github/dekstroza/hopsfactory/orderservice/domain/Order.java
@@ -0,0 +1,153 @@
+package com.github.dekstroza.hopsfactory.orderservice.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity(name = "order")
+@Table(name = "orders", schema = "order_service")
+public class Order implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue
+    private UUID id;
+
+    @Column
+    private UUID inventoryId;
+
+    @Column
+    private UUID customerId;
+
+    @Column
+    private double quantity;
+
+    @Column
+    private double price;
+
+    @Column
+    private String status;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column
+    private Date orderDate;
+
+    public Order() {
+
+    }
+
+    public Order(UUID inventoryId, UUID customerId, double quantity, double totalAmmount, String status, Date orderDate) {
+        this.inventoryId = inventoryId;
+        this.customerId = customerId;
+        this.quantity = quantity;
+        this.price = totalAmmount;
+        this.status = status;
+        this.orderDate = orderDate;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public UUID getInventoryId() {
+        return inventoryId;
+    }
+
+    public UUID getCustomerId() {
+        return customerId;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setInventoryId(UUID inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public void setCustomerId(UUID customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double totalAmmount) {
+        this.price = totalAmmount;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "id=" + id + ", inventoryId=" + inventoryId + ", customerId=" + customerId + ", quantity=" + quantity + ", totalAmmount="
+                + price + ", status='" + status + '\'' + ", orderDate=" + orderDate + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Order order = (Order) o;
+
+        if (Double.compare(order.quantity, quantity) != 0)
+            return false;
+        if (Double.compare(order.price, price) != 0)
+            return false;
+        if (!id.equals(order.id))
+            return false;
+        if (!inventoryId.equals(order.inventoryId))
+            return false;
+        if (!customerId.equals(order.customerId))
+            return false;
+        if (!status.equals(order.status))
+            return false;
+        return orderDate.equals(order.orderDate);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id.hashCode();
+        result = 31 * result + inventoryId.hashCode();
+        result = 31 * result + customerId.hashCode();
+        temp = Double.doubleToLongBits(quantity);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + status.hashCode();
+        result = 31 * result + orderDate.hashCode();
+        return result;
+    }
+}
